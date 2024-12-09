e6d109192161f594172f358d62fbffbf3e15ee30
dotPast <83116565+dotPast@users.noreply.github.com>
2024-12-09 20:03:33 +0300

    oh well looks like kotlin and owo are refusing to cooperate

---
+++ src/client/java/minor/audio/inconvenience/MinorAudioInconvenienceClient.java
@@ -0,0 +1,10 @@
+package minor.audio.inconvenience;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MinorAudioInconvenienceClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
