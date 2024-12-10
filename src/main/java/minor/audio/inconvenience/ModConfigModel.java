e8b5798ed982788d20c19cae6865be17958b153e
dotPast <83116565+dotPast@users.noreply.github.com>
2024-12-10 12:25:53 +0300

    rename the config mai-config -> minor-audio-inconvenience

--- src/main/java/minor/audio/inconvenience/ModConfigModel.java
+++ src/main/java/minor/audio/inconvenience/ModConfigModel.java
@@ -4,7 +4,7 @@ import io.wispforest.owo.config.annotation.Config;
 import io.wispforest.owo.config.annotation.Modmenu;
 
 @Modmenu(modId = "minor-audio-inconvenience")
-@Config(name = "mai-config", wrapperName = "ModConfig")
+@Config(name = "minor-audio-inconvenience", wrapperName = "ModConfig")
 public class ModConfigModel {
     public int anIntOption = 16;
     public boolean aBooleanToggle = false;
