6a1c5cf9de162e7643cb5ef5f3bf4f19913d078f
dotPast <83116565+dotPast@users.noreply.github.com>
2024-12-10 15:36:15 +0300

    add an option for displaying sound ids instead of their names in subtitles

--- src/main/java/minor/audio/inconvenience/ModConfigModel.java
+++ src/main/java/minor/audio/inconvenience/ModConfigModel.java
@@ -9,6 +9,7 @@ import java.util.List;
 @Config(name = "minor-audio-inconvenience", wrapperName = "ModConfig")
 public class ModConfigModel {
     public Boolean enabled = true;
+    public Boolean subtitleId = false;
 
     public List<String> soundList = List.of("minecraft:weather.rain=0");
 }
