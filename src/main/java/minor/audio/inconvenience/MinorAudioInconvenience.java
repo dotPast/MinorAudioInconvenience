8bbc9e98ba4f6bc3413d4ddb5abb28f8bc2769e0
dotPast <83116565+dotPast@users.noreply.github.com>
2024-12-10 13:32:50 +0300

    remove the logger message on init

--- src/main/java/minor/audio/inconvenience/MinorAudioInconvenience.java
+++ src/main/java/minor/audio/inconvenience/MinorAudioInconvenience.java
@@ -16,11 +16,5 @@ public class MinorAudioInconvenience implements ModInitializer {
 	public static final minor.audio.inconvenience.ModConfig CONFIG = minor.audio.inconvenience.ModConfig.createAndLoad();
 
 	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
-	}
+	public void onInitialize() {}
 }
