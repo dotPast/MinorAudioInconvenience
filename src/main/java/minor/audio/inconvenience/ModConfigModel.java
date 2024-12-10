03f1e1fc71006bac2fe74842c59e6e2fa364b87f
dotPast <83116565+dotPast@users.noreply.github.com>
2024-12-10 13:05:54 +0300

    replace the example config with the actual config

--- src/main/java/minor/audio/inconvenience/ModConfigModel.java
+++ src/main/java/minor/audio/inconvenience/ModConfigModel.java
@@ -3,15 +3,12 @@ package minor.audio.inconvenience;
 import io.wispforest.owo.config.annotation.Config;
 import io.wispforest.owo.config.annotation.Modmenu;
 
+import java.util.List;
+
 @Modmenu(modId = "minor-audio-inconvenience")
 @Config(name = "minor-audio-inconvenience", wrapperName = "ModConfig")
 public class ModConfigModel {
-    public int anIntOption = 16;
-    public boolean aBooleanToggle = false;
+    public Boolean enabled = true;
 
-    public Choices anEnumOption = Choices.ANOTHER_CHOICE;
-
-    public enum Choices {
-        A_CHOICE, ANOTHER_CHOICE;
-    }
+    public List<String> soundList = List.of("minecraft:weather.rain=0");
 }
