50350d4cc71d3feeaa2fe3bd5fb3519d12ddd1c6
dotPast <83116565+dotPast@users.noreply.github.com>
2024-12-11 09:45:32 +0300

    replace subtitleId -> showAllSounds

--- src/main/java/minor/audio/inconvenience/ModConfigModel.java
+++ src/main/java/minor/audio/inconvenience/ModConfigModel.java
@@ -2,13 +2,14 @@ package minor.audio.inconvenience;
 
 import io.wispforest.owo.config.annotation.Config;
 import io.wispforest.owo.config.annotation.Modmenu;
+
 import java.util.List;
 
 @Modmenu(modId = "minor-audio-inconvenience")
 @Config(name = "minor-audio-inconvenience", wrapperName = "ModConfig")
 public class ModConfigModel {
     public Boolean enabled = true;
-    public Boolean subtitleId = false;
-    
+    public Boolean showAllSounds = false;
+
     public List<String> soundList = List.of("minecraft:weather.rain=0");
 }
