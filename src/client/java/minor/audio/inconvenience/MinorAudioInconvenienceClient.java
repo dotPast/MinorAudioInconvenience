da34ae46290feb85e2a5306ec769fb3dfd9e8a92
dotPast <83116565+dotPast@users.noreply.github.com>
2024-12-11 21:36:37 +0300

    remove the debug keybind to open the force set sound volume menu

--- src/client/java/minor/audio/inconvenience/MinorAudioInconvenienceClient.java
+++ src/client/java/minor/audio/inconvenience/MinorAudioInconvenienceClient.java
@@ -1,28 +1,8 @@
 package minor.audio.inconvenience;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
-import org.lwjgl.glfw.GLFW;
 
 public class MinorAudioInconvenienceClient implements ClientModInitializer {
 	@Override
-	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-
-		KeyBinding keyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-				"key.examplemod.spook", // The translation key of the keybinding's name
-				InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
-				GLFW.GLFW_KEY_APOSTROPHE, // The keycode of the key
-				"category.examplemod.test" // The translation key of the keybinding's category.
-		));
-
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-			while (keyBinding.wasPressed()) {
-				client.setScreen(new AddSoundsScreen());
-			}
-		});
-	}
+	public void onInitializeClient() {}
 }
