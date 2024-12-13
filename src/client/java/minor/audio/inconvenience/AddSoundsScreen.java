6a91d5a300a88a9e6153ee59b81c8d327d731a8f
dotPast <83116565+dotPast@users.noreply.github.com>
2024-12-13 10:56:39 +0300

    add a search bar, split generating the menu into several functions, fix the text alignment

--- src/client/java/minor/audio/inconvenience/AddSoundsScreen.java
+++ src/client/java/minor/audio/inconvenience/AddSoundsScreen.java
@@ -4,6 +4,7 @@ import io.wispforest.owo.ui.base.BaseOwoScreen;
 import io.wispforest.owo.ui.component.ButtonComponent;
 import io.wispforest.owo.ui.component.Components;
 import io.wispforest.owo.ui.component.SliderComponent;
+import io.wispforest.owo.ui.component.TextBoxComponent;
 import io.wispforest.owo.ui.container.CollapsibleContainer;
 import io.wispforest.owo.ui.container.Containers;
 import io.wispforest.owo.ui.container.FlowLayout;
@@ -29,117 +30,28 @@ public class AddSoundsScreen extends BaseOwoScreen<FlowLayout> {
 
     @Override
     protected void build(FlowLayout root) {
-        MinecraftClient gameInstance = MinecraftClient.getInstance();
-        SoundManager soundManager = gameInstance.getSoundManager();
-
         root.surface(Surface.VANILLA_TRANSLUCENT)
                 .horizontalAlignment(HorizontalAlignment.CENTER)
                 .verticalAlignment(VerticalAlignment.CENTER);
 
         FlowLayout soundList = Containers.verticalFlow(Sizing.fill(), Sizing.content());
+        FlowLayout header = Containers.horizontalFlow(Sizing.fill(), Sizing.content());
         FlowLayout soundLayout = Containers.verticalFlow(Sizing.content(), Sizing.content());
 
-        Map<String, CollapsibleContainer> containers = new HashMap<>();
+        TextBoxComponent searchBar = Components.textBox(Sizing.expand()).text("");
 
-        for (Identifier soundID : soundManager.sounds.keySet()) {
-            String soundNamespace = soundID.getNamespace();
-            String[] splitSoundPath = soundID.getPath().split("\\.");
+        generateEntries(soundLayout, searchBar.getText());
 
-            CollapsibleContainer container;
+        searchBar.onChanged().subscribe((query) -> {
+            soundLayout.clearChildren();
+            generateEntries(soundLayout, query);
+        });
 
-            if (containers.isEmpty() || !containers.containsKey(soundNamespace)) {
-                container = Containers.collapsible(Sizing.content(), Sizing.content(), Text.literal(soundNamespace), false);
-            } else {
-                container = containers.get(soundNamespace);
-            }
+        header.child(Components.label(Text.translatable("text.menu.minor-audio-inconvenience.add.title")).verticalTextAlignment(VerticalAlignment.CENTER).sizing(Sizing.content(), Sizing.fixed(20)));
+        header.child(Components.label(Text.translatable("text.menu.minor-audio-inconvenience.add.search").append(":")).verticalTextAlignment(VerticalAlignment.CENTER).sizing(Sizing.content(), Sizing.fixed(20)).margins(Insets.left(16)));
+        header.child(searchBar.margins(Insets.left(4)));
 
-            FlowLayout soundOption = Containers.horizontalFlow(Sizing.content(), Sizing.content());
-
-            soundOption.child(
-                    Components.button(Text.literal("▶"), button -> {
-                        SoundInstance soundInstance;
-                        if (gameInstance.getServer() != null || gameInstance.getCurrentServerEntry() != null) {
-                            if (gameInstance.player != null) {
-                                soundInstance = new PositionedSoundInstance(
-                                        new SoundEvent(soundID, null), 
-                                        SoundCategory.MASTER,
-                                        1f,
-                                        1f,
-                                        Random.create(),
-                                        gameInstance.player.getX(),
-                                        gameInstance.player.getY(),
-                                        gameInstance.player.getZ()
-                                );
-                            } else {
-                                soundInstance = new AbstractSoundInstance(
-                                        new SoundEvent(soundID, null),
-                                        SoundCategory.MASTER,
-                                        Random.create()
-                                ) {};
-                            }
-                        } else {
-                            soundInstance = new AbstractSoundInstance(
-                                    new SoundEvent(soundID, null),
-                                    SoundCategory.MASTER,
-                                    Random.create()
-                            ) {};
-                        }
-
-                        gameInstance.getSoundManager().play(soundInstance);
-                    })
-                            .margins(Insets.of(0, 0, 0, 16))
-            );
-
-            soundOption.child(
-                    Components.label(Text.literal(soundID.getPath()))
-            );
-
-            SliderComponent volumeSlider = Components.slider(Sizing.fill(10)).message(
-                    (String progress) -> Text.translatable("text.menu.minor-audio-inconvenience.add.volumeslider.value").append(Text.literal(String.format(
-                            ": %s",
-                            (int) Math.ceil(Double.parseDouble(progress) * 100)
-                    )).append("%"))
-            ).value(1).scrollStep(0.01);
-
-            soundOption.child(
-                    volumeSlider.margins(Insets.of(0, 0, 16, 16))
-            );
-
-            ButtonComponent addToConfigButton = Components.button(Text.translatable("text.menu.minor-audio-inconvenience.add.button.addtoconfig"), button -> {
-                Double volume = Math.ceil(volumeSlider.value() * 100) / 100;
-
-                for (String configId : MinorAudioInconvenience.CONFIG.soundList()) {
-                    if (configId.split("=")[0].equals(soundID.toString())) {
-                        MinorAudioInconvenience.CONFIG.soundList().remove(configId);
-                        break;
-                    }
-                }
-
-                MinorAudioInconvenience.CONFIG.soundList().add(String.format("%s=%s", soundID, volume));
-                MinorAudioInconvenience.CONFIG.save();
-                button.setMessage(Text.translatable("text.menu.minor-audio-inconvenience.add.button.addtoconfig.success"));
-            });
-
-            for (String configId : MinorAudioInconvenience.CONFIG.soundList()) {
-                String[] configSplit = configId.split("=");
-
-                if (configSplit[0].equals(soundID.toString())) {
-                    volumeSlider.value(Double.parseDouble(configSplit[1]));
-                }
-            }
-
-            soundOption.child(addToConfigButton);
-
-            container.child(soundOption);
-
-            containers.put(soundNamespace, container);
-        }
-
-        for (CollapsibleContainer container : containers.values()) {
-            soundLayout.child(container);
-        }
-
-        soundList.child(Components.label(Text.translatable("text.menu.minor-audio-inconvenience.add.title")).horizontalTextAlignment(HorizontalAlignment.CENTER));
+        soundList.child(header);
         soundList.child(soundLayout);
 
         root.child(
@@ -152,5 +64,135 @@ public class AddSoundsScreen extends BaseOwoScreen<FlowLayout> {
                         .verticalAlignment(VerticalAlignment.CENTER)
                         .horizontalAlignment(HorizontalAlignment.CENTER)
         );
+    }
+
+    public void generateEntries(FlowLayout layout, String searchQuery) {
+        MinecraftClient gameInstance = MinecraftClient.getInstance();
+        SoundManager soundManager = gameInstance.getSoundManager();
+
+        Map<String, CollapsibleContainer> containers = new HashMap<>();
+
+        for (Identifier soundID : soundManager.sounds.keySet()) {
+            if (searchQuery.length() == 0) {
+                String soundNamespace = soundID.getNamespace();
+
+                CollapsibleContainer container;
+
+                if (containers.isEmpty() || !containers.containsKey(soundNamespace)) {
+                    container = Containers.collapsible(Sizing.content(), Sizing.content(), Text.literal(soundNamespace), false);
+                } else {
+                    container = containers.get(soundNamespace);
+                }
+
+                container.child(generateSoundComponent(soundID));
+
+                containers.put(soundNamespace, container);
+            } else {
+                if (soundID.getPath().contains(searchQuery)) {
+                    String soundNamespace = soundID.getNamespace();
+
+                    CollapsibleContainer container;
+
+                    if (containers.isEmpty() || !containers.containsKey(soundNamespace)) {
+                        container = Containers.collapsible(Sizing.content(), Sizing.content(), Text.literal(soundNamespace), false);
+                    } else {
+                        container = containers.get(soundNamespace);
+                    }
+
+                    container.child(generateSoundComponent(soundID));
+
+                    containers.put(soundNamespace, container);
+                }
+            }
+        }
+
+        for (CollapsibleContainer container : containers.values()) {
+            layout.child(container);
+        }
+    }
+
+    public FlowLayout generateSoundComponent(Identifier soundID) {
+        MinecraftClient gameInstance = MinecraftClient.getInstance();
+        SoundManager soundManager = gameInstance.getSoundManager();
+
+        FlowLayout soundOption = Containers.horizontalFlow(Sizing.content(), Sizing.content());
+
+        soundOption.child(
+                Components.button(Text.literal("▶"), button -> {
+                            SoundInstance soundInstance;
+                            if (gameInstance.getServer() != null || gameInstance.getCurrentServerEntry() != null) {
+                                if (gameInstance.player != null) {
+                                    soundInstance = new PositionedSoundInstance(
+                                            new SoundEvent(soundID, null),
+                                            SoundCategory.MASTER,
+                                            1f,
+                                            1f,
+                                            Random.create(),
+                                            gameInstance.player.getX(),
+                                            gameInstance.player.getY(),
+                                            gameInstance.player.getZ()
+                                    );
+                                } else {
+                                    soundInstance = new AbstractSoundInstance(
+                                            new SoundEvent(soundID, null),
+                                            SoundCategory.MASTER,
+                                            Random.create()
+                                    ) {};
+                                }
+                            } else {
+                                soundInstance = new AbstractSoundInstance(
+                                        new SoundEvent(soundID, null),
+                                        SoundCategory.MASTER,
+                                        Random.create()
+                                ) {};
+                            }
+
+                            soundManager.play(soundInstance);
+                        })
+                        .margins(Insets.of(0, 0, 0, 16))
+                        .sizing(Sizing.fixed(20))
+        );
+
+        soundOption.child(
+                Components.label(Text.literal(soundID.getPath())).verticalTextAlignment(VerticalAlignment.CENTER).sizing(Sizing.content(), Sizing.fixed(20))
+        );
+
+        SliderComponent volumeSlider = Components.slider(Sizing.fill(10)).message(
+                (String progress) -> Text.translatable("text.menu.minor-audio-inconvenience.add.volumeslider.value").append(Text.literal(String.format(
+                        ": %s",
+                        (int) Math.ceil(Double.parseDouble(progress) * 100)
+                )).append("%"))
+        ).value(1).scrollStep(0.01);
+
+        soundOption.child(
+                volumeSlider.margins(Insets.of(0, 0, 16, 16))
+        );
+
+        ButtonComponent addToConfigButton = Components.button(Text.translatable("text.menu.minor-audio-inconvenience.add.button.addtoconfig"), button -> {
+            Double volume = Math.ceil(volumeSlider.value() * 100) / 100;
+
+            for (String configId : MinorAudioInconvenience.CONFIG.soundList()) {
+                if (configId.split("=")[0].equals(soundID.toString())) {
+                    MinorAudioInconvenience.CONFIG.soundList().remove(configId);
+                    break;
+                }
+            }
+
+            MinorAudioInconvenience.CONFIG.soundList().add(String.format("%s=%s", soundID, volume));
+            MinorAudioInconvenience.CONFIG.save();
+            button.setMessage(Text.translatable("text.menu.minor-audio-inconvenience.add.button.addtoconfig.success"));
+        });
+
+        for (String configId : MinorAudioInconvenience.CONFIG.soundList()) {
+            String[] configSplit = configId.split("=");
+
+            if (configSplit[0].equals(soundID.toString())) {
+                volumeSlider.value(Double.parseDouble(configSplit[1]));
+            }
+        }
+
+        soundOption.child(addToConfigButton);
+
+        return soundOption;
     }
 }
