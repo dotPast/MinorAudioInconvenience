7eeaffaafba766a14b3e454f0c4d1c3c3b65941a
dotPast <83116565+dotPast@users.noreply.github.com>
2024-12-17 14:42:15 +0300

    show the owo config screen when opening config through the mod menu

--- src/main/java/minor/audio/inconvenience/MinorAudioInconvenience.java
+++ src/main/java/minor/audio/inconvenience/MinorAudioInconvenience.java
@@ -1,18 +1,22 @@
 package minor.audio.inconvenience;
 
 import com.mojang.logging.LogUtils;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import org.slf4j.Logger;
+import minor.audio.inconvenience.ModConfig;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
-@Mod(MinorAudioInconvenience.MODID)
+@Mod(value = MinorAudioInconvenience.MODID, dist = Dist.CLIENT)
 public class MinorAudioInconvenience {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "minoraudioinconvenience";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
+
+    public static final ModConfig CONFIG = ModConfig.createAndLoad();
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
--- src/main/java/minor/audio/inconvenience/ModConfigModel.java
+++ src/main/java/minor/audio/inconvenience/ModConfigModel.java
@@ -1,9 +1,11 @@
 package minor.audio.inconvenience;
 
 import io.wispforest.owo.config.annotation.Config;
+import io.wispforest.owo.config.annotation.Modmenu;
 
 import java.util.List;
 
+@Modmenu(modId = "minoraudioinconvenience")
 @Config(name = "minor-audio-inconvenience", wrapperName = "ModConfig")
 public class ModConfigModel {
     public Boolean enabled = true;
