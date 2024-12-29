b204de5d6b89b62ff9fb2395ca14a6bb3f877560
dotPast <83116565+dotPast@users.noreply.github.com>
2024-12-29 15:59:29 +0300

    rewrite PlaySoundMixin to set the volume on the sound system level
    the original implementation only supported injecting volume into sounds which go through the client's world. some mods (ie. IslandUtils by AsoDesu) play sounds directly through the sound manager. the new implementation injects volume in the sound system call. it allows to inject volume on all sounds regardless if they go through the client's world

--- src/client/java/minor/audio/inconvenience/mixin/client/PlaySoundMixin.java
+++ src/client/java/minor/audio/inconvenience/mixin/client/PlaySoundMixin.java
@@ -1,34 +1,31 @@
 package minor.audio.inconvenience.mixin.client;
 
 import minor.audio.inconvenience.MinorAudioInconvenience;
-import net.minecraft.client.sound.PositionedSoundInstance;
-import net.minecraft.client.world.ClientWorld;
+import net.minecraft.client.sound.*;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.math.random.Random;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
-import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 import java.util.Objects;
 
-@Mixin(ClientWorld.class)
+@Mixin(SoundSystem.class)
 public class PlaySoundMixin {
-    @ModifyArgs(method = "playSound(DDDLnet/minecraft/sound/SoundEvent;Lnet/minecraft/sound/SoundCategory;FFZJ)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/sound/SoundManager;play(Lnet/minecraft/client/sound/SoundInstance;I)V"))
-    private void injectVolume(Args args) {
+    @ModifyVariable(method = "play(Lnet/minecraft/client/sound/SoundInstance;)V", at = @At("HEAD"), argsOnly = true)
+    private SoundInstance injectVolume(SoundInstance sound) {
+        SoundInstance modifiedSound = sound;
         if (MinorAudioInconvenience.CONFIG.enabled()) {
-            PositionedSoundInstance sound = args.get(0);
-
             float volume = 1;
             float pitch = 1;
 
             try {
-                volume = sound.getVolume();
+                volume = modifiedSound.getVolume();
             } catch (Exception ignored) {
             }
 
             try {
-                pitch = sound.getPitch();
+                pitch = modifiedSound.getPitch();
             } catch (Exception ignored) {
             }
 
@@ -37,7 +34,7 @@ public class PlaySoundMixin {
                 try {
                     String[] soundOverrideId = splitSoundOverride[0].split(":");
 
-                    if (Objects.equals(sound.getId().getNamespace(), soundOverrideId[0]) && Objects.equals(sound.getId().getPath(), soundOverrideId[1])) {
+                    if (Objects.equals(modifiedSound.getId().getNamespace(), soundOverrideId[0]) && Objects.equals(modifiedSound.getId().getPath(), soundOverrideId[1])) {
                         volume = Float.parseFloat(splitSoundOverride[1]);
                     }
                 } catch (Exception ignored) {
@@ -45,52 +42,11 @@ public class PlaySoundMixin {
                 }
             }
 
-            args.set(
-                    0,
-                    new PositionedSoundInstance(
-                            SoundEvent.of(sound.getId()), sound.getCategory(), volume, pitch, Random.create(), sound.getX(), sound.getY(), sound.getZ()
-                    )
+            modifiedSound = new PositionedSoundInstance(
+                    SoundEvent.of(sound.getId()), sound.getCategory(), volume, pitch, Random.create(), sound.getX(), sound.getY(), sound.getZ()
             );
         }
-    }
 
-    @ModifyArgs(method = "playSound(DDDLnet/minecraft/sound/SoundEvent;Lnet/minecraft/sound/SoundCategory;FFZJ)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/sound/SoundManager;play(Lnet/minecraft/client/sound/SoundInstance;)V"))
-    private void injectVolumeNoDelay(Args args) {
-        if (MinorAudioInconvenience.CONFIG.enabled()) {
-            PositionedSoundInstance sound = args.get(0);
-
-            float volume = 1;
-            float pitch = 1;
-
-            try {
-                volume = sound.getVolume();
-            } catch (Exception ignored) {
-            }
-
-            try {
-                pitch = sound.getPitch();
-            } catch (Exception ignored) {
-            }
-
-            for (String soundOverride : MinorAudioInconvenience.CONFIG.soundList()) {
-                String[] splitSoundOverride = soundOverride.split("=");
-                try {
-                    String[] soundOverrideId = splitSoundOverride[0].split(":");
-
-                    if (Objects.equals(sound.getId().getNamespace(), soundOverrideId[0]) && Objects.equals(sound.getId().getPath(), soundOverrideId[1])) {
-                        volume = Float.parseFloat(splitSoundOverride[1]);
-                    }
-                } catch (Exception ignored) {
-                    MinorAudioInconvenience.LOGGER.error("Format of '{}' sound override is invalid.", soundOverride);
-                }
-            }
-
-            args.set(
-                    0,
-                    new PositionedSoundInstance(
-                            SoundEvent.of(sound.getId()), sound.getCategory(), volume, pitch, Random.create(), sound.getX(), sound.getY(), sound.getZ()
-                    )
-            );
-        }
+        return modifiedSound;
     }
 }
