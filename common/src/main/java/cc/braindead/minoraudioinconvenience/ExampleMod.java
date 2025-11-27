197b742a858bb38cc2b3f06674efc13ae42f8981
dotundrscr <83116565+dotundrscr@users.noreply.github.com>
2025-11-27 20:07:10 +0300

    init

---
+++ common/src/main/java/cc/braindead/minoraudioinconvenience/ExampleMod.java
@@ -0,0 +1,9 @@
+package cc.braindead.minoraudioinconvenience;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "minoraudioinconvenience";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
---
+++ neoforge/src/main/java/cc/braindead/minoraudioinconvenience/neoforge/ExampleModNeoForge.java
@@ -0,0 +1,13 @@
+package cc.braindead.minoraudioinconvenience.neoforge;
+
+import net.neoforged.fml.common.Mod;
+
+import cc.braindead.minoraudioinconvenience.ExampleMod;
+
+@Mod(ExampleMod.MOD_ID)
+public final class ExampleModNeoForge {
+    public ExampleModNeoForge() {
+        // Run our common setup.
+        ExampleMod.init();
+    }
+}
